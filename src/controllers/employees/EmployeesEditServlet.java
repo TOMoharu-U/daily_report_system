c09f57347c1e9c61518f69a564108f4078582377
TOM.U <txm.money@gmail.com>
2019-08-04 18:28:21 +0900

    Add Employee show

--- src/controllers/employees/EmployeesEditServlet.java
+++ src/controllers/employees/EmployeesEditServlet.java
@@ -8,16 +8,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class editServlet
+ * Servlet implementation class EmployeesEditServlet
  */
 @WebServlet("/employees/edit")
-public class editServlet extends HttpServlet {
+public class EmployeesEditServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public editServlet() {
+    public EmployeesEditServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
