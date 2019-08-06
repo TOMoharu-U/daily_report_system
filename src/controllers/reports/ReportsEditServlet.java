da1439e0208113317c55d51cedad504efd77cb6f
TOM.U <txm.money@gmail.com>
2019-08-06 09:35:56 +0900

    Add Report index

--- src/controllers/reports/ReportsEditServlet.java
+++ src/controllers/reports/ReportsEditServlet.java
@@ -1,27 +1,23 @@
-package controllers.employees;
+package controllers.reports;
 
 import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import models.Employee;
-
 /**
- * Servlet implementation class NewServlet
+ * Servlet implementation class ReportsEditServlet
  */
-@WebServlet("/employees/new")
-public class EmployeesNewServlet extends HttpServlet {
+@WebServlet("/reports/edit")
+public class ReportsEditServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+       
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public EmployeesNewServlet() {
+    public ReportsEditServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -30,11 +26,16 @@ public class EmployeesNewServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setAttribute("_token", request.getSession().getId());
-		request.setAttribute("employee", new Employee());
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
 
-		RequestDispatcher rd =request.getRequestDispatcher("/WEB-INF/views/employees/new.jsp");
-		rd.forward(request, response);
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
 	}
 
 }
--- src/controllers/reports/ReportsIndexServlet.java
+++ src/controllers/reports/ReportsIndexServlet.java
@@ -1,4 +1,5 @@
-package controllers.employees;
+
+package controllers.reports;
 
 import java.io.IOException;
 import java.util.List;
@@ -11,20 +12,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import models.Employee;
+import models.Report;
 import utils.DBUtil;
 
+
 /**
- * Servlet implementation class IndexServlet
+ * Servlet implementation class ReportsIndexServlet
  */
-@WebServlet("/employees/index")
-public class EmployeesIndexServlet extends HttpServlet {
+@WebServlet("/reports/index")
+public class ReportsIndexServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public EmployeesIndexServlet() {
+    public ReportsIndexServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -35,29 +37,32 @@ public class EmployeesIndexServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		EntityManager em = DBUtil.createEntityManager();
 
-		int page = 1;
+		int page;
 		try{
 			page = Integer.parseInt(request.getParameter("page"));
-		} catch(NumberFormatException e) { }
-		List<Employee>employees = em.createNamedQuery("getAllEmployees", Employee.class)
-									.setFirstResult(15 * (page - 1))
-									.setMaxResults(15)
-									.getResultList();
-		long employees_count = (long)em.createNamedQuery("getEmployeesCount", Long.class)
-										.getSingleResult();
+		} catch(Exception e) {
+			page = 1;
+		}
+		List<Report> reports = em.createNamedQuery("getAllReport", Report.class)
+								.setFirstResult(15 * (page - 1))
+								.setMaxResults(15)
+								.getResultList();
+
+		long reports_count = (long)em.createNamedQuery("getAllReportsCount", Long.class )
+									.getSingleResult();
 
 		em.close();
 
-		request.setAttribute("employees", employees);
-		request.setAttribute("employees_count", employees_count);
+		request.setAttribute("reports", reports);
+		request.setAttribute("reports_count", reports_count);
 		request.setAttribute("page", page);
 		if(request.getSession().getAttribute("flush") != null) {
-			request.setAttribute("flush", request.getSession().getAttribute("flush"));
-			request.getSession().removeAttribute("flush");
+			request.setAttribute("flish", request.getSession().getAttribute("flush"));
+			request.getSession().removeAttribute("flish");
 		}
 
-		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/employees/index.jsp");
-        rd.forward(request, response);
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/index.jsp");
+		rd.forward(request, response);
 	}
 
 }
