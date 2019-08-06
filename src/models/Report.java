62a7e5e35df21a7275eb74293e8f7eba0ce071a5
TOM.U <txm.money@gmail.com>
2019-08-06 12:14:44 +0900

    Add Report create

--- src/models/Report.java
+++ src/models/Report.java
@@ -73,7 +73,7 @@ public class Report {
 		return report_date;
 	}
 
-	public void getReport_date(Date report_date) {
+	public void setReport_date(Date report_date) {
 		this.report_date = report_date;
 	}
 
